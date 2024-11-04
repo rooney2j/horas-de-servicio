7f038e9f312e3ef0b5c1ac4696583e427302d0fc
CF-Tuc <carlosfaciano6@gmail.com>
2024-11-04 16:29:39 -0300

    login

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,16 +1,37 @@
+"use client"
 import React from 'react'
-import { cookies } from 'next/headers'
+import { signIn } from 'next-auth/react'
 export default function Login() {
+
+  
+
+  const handleSubmit = async(e)=>{
+     e.preventDefault();
+     const formData = new FormData(e.target)
+     console.log(formData.get("email"))
+     console.log(formData.get("password"))
+      const result = await signIn("credentials",{
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirectTo:"/" 
+      
+     }) 
+    
+  }
+
   return (
     <div className='h-screen w-screen bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 flex items-center justify-center'>
-        <section className='w-1/2 h-3/5 bg-sky-200 rounded-lg flex '>
-          <article className='w-1/2 bg-sky-200 h-full flex flex-col items-center'>
-            <h2 className='font-bold text-blue-950 text-3xl mt-10'> Iniciar Sesion</h2>
-            <input className='h-10 w-4/5 rounded-lg outline-none ps-2 mt-16 font-medium' placeholder='Nombre de Usuario' type="text" />
-            <input className='h-10 w-4/5 rounded-lg outline-none ps-2 mt-16 font-medium' placeholder='Contraseña' type="text" />
-            <button className='h-9 w-32 rounded-lg bg-sky-500 hover:duration-300 hover:ease-out hover:bg-sky-600 text-white font-medium mt-10 active:duration-300 active:ease-out active:w-28 active:h-8 active:bg-blue-700'>Enviar</button>
-          </article>
-          <article className='w-1/2 h-full flex justify-center items-center rounded-es-[5rem] bg-white'>
+        <section className='w-full h-full md:w-4/5 md:h-3/5 lg:w-1/2 lg:h-3/5 bg-sky-200 rounded-lg flex flex-wrap '>
+          <form onSubmit={handleSubmit} className='w-full bg-blue-500 md:w-1/2 md:bg-sky-200 h-full flex flex-col items-center'>
+            <h2 className='hidden md:flex font-bold text-blue-950 text-3xl mt-10'> Iniciar Sesion</h2>
+             <div className='flex md:hidden '>
+               <img className='mix-blend-multiply' src="https://www.estudiantefunval.org/pluginfile.php/1/theme_moove/logo/1729720886/logo%202023.png" alt="" />
+             </div>
+            <input name='email' className='h-10 w-4/5 rounded-lg outline-none ps-2 mt-16 font-medium' placeholder='Nombre de Usuario' type="text" />
+            <input name='password' className='h-10 w-4/5 rounded-lg outline-none ps-2 mt-16 font-medium' placeholder='Contraseña' type="text" />
+            <button type='submit' className='h-9 w-32 rounded-lg bg-sky-500 hover:duration-300 hover:ease-out hover:bg-sky-600 text-white font-medium mt-10 active:duration-300 active:ease-out active:w-28 active:h-8 active:bg-blue-700'>Enviar</button>
+          </form>
+          <article className='hidden md:flex w-full md:w-1/2 h-full  justify-center items-center rounded-es-[5rem] bg-white'>
             <img src="https://www.estudiantefunval.org/pluginfile.php/1/theme_moove/logo/1729720886/logo%202023.png" alt="" />
           </article>
         </section>
